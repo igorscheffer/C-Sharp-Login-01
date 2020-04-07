560bd112a50efb637f0d6dcb9add99f50eedf526
Igor Scheffer Ferreira <igorschefferferreira@hotmail.com.br>
2020-04-07 16:42:25 -0300

    design Logado

--- Login/Logado.Designer.cs
+++ Login/Logado.Designer.cs
@@ -29,26 +29,44 @@
         private void InitializeComponent()
         {
             this.mensagem = new System.Windows.Forms.Label();
+            this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // mensagem
             // 
             this.mensagem.AutoSize = true;
             this.mensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mensagem.Location = new System.Drawing.Point(190, 205);
+            this.mensagem.Location = new System.Drawing.Point(215, 213);
             this.mensagem.Name = "mensagem";
             this.mensagem.Size = new System.Drawing.Size(568, 46);
             this.mensagem.TabIndex = 0;
             this.mensagem.Text = "Usuario Logado com Sucesso.";
+            // 
+            // iconPictureBox1
+            // 
+            this.iconPictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.iconPictureBox1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.iconPictureBox1.IconChar = FontAwesome.Sharp.IconChar.Check;
+            this.iconPictureBox1.IconColor = System.Drawing.Color.ForestGreen;
+            this.iconPictureBox1.IconSize = 100;
+            this.iconPictureBox1.Location = new System.Drawing.Point(449, 110);
+            this.iconPictureBox1.Name = "iconPictureBox1";
+            this.iconPictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.iconPictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.iconPictureBox1.TabIndex = 1;
+            this.iconPictureBox1.TabStop = false;
             // 
             // Logado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(987, 485);
+            this.Controls.Add(this.iconPictureBox1);
             this.Controls.Add(this.mensagem);
             this.Name = "Logado";
             this.Text = "Logado";
+            ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +75,6 @@
         #endregion
 
         private System.Windows.Forms.Label mensagem;
+        private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
     }
 }
